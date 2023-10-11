f62a72a488e304c643cdaa654dc4af19ed2236d7
0xMaina <bennymmwangi@gmail.com>
2023-10-11 04:52:52 -0700

    push

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result = [[0 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-             result[i][j] = matrix[i][j] ** 2
-
-    return result
+    if isinstance(matrix, list):
+        if len(matrix) == 0:
+            return (matrix)
+        new_matrix = matrix.copy()
+        for i in range(len(matrix)):
+            new_matrix[i] = list(map(lambda n: n**2, matrix[i]))
+        return (new_matrix)
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
+    if key not in list(a_dictionary.keys()):
+        return (a_dictionary)
+    if a_dictionary:
+        for k in list(a_dictionary.keys()):
+            if k == key:
+                del a_dictionary[k]
+                break
+            else:
+                continue
+        return (a_dictionary)
